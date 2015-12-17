cbd82f6d03dc9a6871a7221414dfb638ceae0ee1
Samy Pesse <samypesse@gmail.com>
2015-12-17 08:38:02 +0100

    Base for log command

--- bin/log.js
+++ bin/log.js
@@ -3,9 +3,24 @@ var repo = require('./repo');
 
 program
     .command('log')
+    .option('-l, --limit', 'maximum count of commits to show')
     .action(function() {
         var head = repo.Head();
 
-        head.parse()
+        function printCommit(commit) {
+            return commit.parse()
+            .then(function() {
+                console.log(commit.sha);
+                console.log('');
+            });
+        }
 
+        head.resolve()
+        .then(function(ref) {
+            return ref.resolveToCommit();
+        })
+        .then(function(commit) {
+            return printCommit(commit);
+        })
+        .fail(console.log.bind(console));
     });
--- test.js
+++ test.js
@@ -6,6 +6,6 @@ var repo = new GitRepo(fs);
 
 var log = console.log.bind(console);
 
-repo.object('a5e766951d4255211fe34175bb14dcd90954a83b').read()
+repo.commit('a5e766951d4255211fe34175bb14dcd90954a83b').parse()
 .then(log, log);
 
