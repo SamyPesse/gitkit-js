5409a4caef845fe8303d84a18d5a59db9c36ae6e
Samy Pessé <samypesse@gmail.com>
2015-12-17 11:46:37 +0100

    Complete log command

--- bin/log.js
+++ bin/log.js
@@ -3,17 +3,24 @@ var repo = require('./repo');
 
 program
     .command('log')
-    .option('-l, --limit', 'maximum count of commits to show')
+    .option('-l, --limit [limit]', 'maximum count of commits to show')
     .action(function() {
         var head = repo.Head();
+        var limit = this.limit || 10;
+        var index = 0;
 
         function printCommit(commit) {
+            index = index + 1;
+
             return commit.parse()
             .then(function() {
                 console.log(commit.sha);
                 console.log(commit.author.toString())
                 console.log(commit.message);
                 console.log('');
+
+                if (commit.parents.length == 0 || index >= limit) return;
+                return printCommit(commit.parents[0]);
             });
         }
 
