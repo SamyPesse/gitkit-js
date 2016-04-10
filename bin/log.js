8bddf06aa41e5b2db8ee1456a4966a876c1241f8
Samy Pesse <samypesse@gmail.com>
2016-04-10 11:56:25 +0200

    Limit log command

--- bin/log.js
+++ bin/log.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-console */
+
 var git = require('../');
 var command = require('./command');
 
 module.exports = command('log', function(repo, args) {
+    var MAX = 100;
+    var i = 0;
+
     function logCommit(sha, commit, depth) {
+        i++;
+
         console.log(sha);
         console.log(commit.getAuthor().toString());
         console.log(commit.getMessage());
         console.log('');
+
+        if (i >= MAX) return false;
     }
 
     return git.Head.readFromRepo(repo)
