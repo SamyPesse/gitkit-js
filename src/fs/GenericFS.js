d3e14d6e648271ccbda996ed31e170c04f0786ce
Samy Pessé <samypesse@gmail.com>
2017-06-19 21:09:19 +0200

    Add method readTree on fs to list all files

--- src/fs/GenericFS.js
+++ src/fs/GenericFS.js
@@ -1,5 +1,6 @@
 /** @flow */
 
+import path from 'path';
 import type { List } from 'immutable';
 
 export type FileType = 'file' | 'dir';
@@ -12,39 +13,68 @@ export type FileStat = {
 };
 
 class GenericFS {
+
     /*
      * List files in a folder.
      */
-    readDir(path: string): Promise<List<string>> {
+    readDir(dirpath: string): Promise<List<string>> {
         return Promise.reject(new Error('Not implemented'));
     }
 
     /*
      * Get infos about a file.
      */
-    stat(path: string): Promise<FileStat> {
+    stat(filepath: string): Promise<FileStat> {
         return Promise.reject(new Error('Not implemented'));
     }
 
     /*
      * Read a file.
      */
-    read(path: string): Promise<Buffer> {
+    read(filepath: string): Promise<Buffer> {
         return Promise.reject(new Error('Not implemented'));
     }
 
     /*
      * Write a file.
      */
-    write(path: string, content: Buffer): Promise<*> {
+    write(filepath: string, content: Buffer): Promise<*> {
         return Promise.reject(new Error('Not implemented'));
     }
 
     /*
      * Delete a file.
      */
-    unlink(path: string): Promise<*> {
+    unlink(filepath: string): Promise<*> {
         return Promise.reject(new Error('Not implemented'));
+    }
+
+    /*
+     * List all files in a tree.
+     */
+    readTree(dirpath: string = ''): OrderedMap<string,FileStat> {
+        return this.readDir(dirpath)
+        .then(files => {
+
+            return files.reduce(
+                (prev, file) => {
+                    return prev.then((accu) => {
+                        const filepath = path.join(dirpath, file);
+
+                        return this.stat(filepath)
+                        .then(stat => {
+                            if (stat.type == 'dir') {
+                                return this.readTree(filepath)
+                                .then(out => accu.merge(out))
+                            }
+
+                            return accu.set(filepath, stat);
+                        })
+                    });
+                },
+                Promise.resolve(new OrderedMap())
+            );
+        });
     }
 }
 
