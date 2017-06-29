8e3c4fb48eaf12223c50ead9d655a69197c8f3d7
Samy Pessé <samypesse@gmail.com>
2017-06-29 12:21:44 +0200

    Fix unit tests for HEAD/Ref

--- src/models/__tests__/Ref.js
+++ src/models/__tests__/Ref.js
@@ -28,21 +28,3 @@ describe('createFromBuffer', () => {
         expect(head.commit).toBe(null);
     });
 });
-
-describe('isDetached', () => {
-    test('it should be true when pointing to a commit', () => {
-        const head = new Ref({
-            commit: '8a3493eea604510d7ba532cce73bf0fb68c6db8f'
-        });
-
-        expect(head.isDetached).toBe(true);
-    });
-
-    test('it should be false when pointing to a ref', () => {
-        const head = new Ref({
-            ref: 'refs/heads/master'
-        });
-
-        expect(head.isDetached).toBe(false);
-    });
-});
